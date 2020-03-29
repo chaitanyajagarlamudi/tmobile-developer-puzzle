ab4ba5360c16019fd8eea56e8a8cdbd75f41647b
chaitanya <chaitanyajagar@gmail.com>
2020-03-29 02:40:48 -0700

    commit for token intigration

--- libs/shared/ui/chart/src/lib/chart/chart.component.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.ts
@@ -34,6 +34,9 @@ export class ChartComponent implements OnInit {
       options: { title: `Stock price`, width: '600', height: '400' }
     };
 
-    this.data$.subscribe(newData => (this.chartData = newData));
+    this.data$.subscribe(newData => {
+      console.log('=========', newData)
+      this.chartData = newData
+    });
   }
 }
